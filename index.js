614b6ee28064e8f9410ccb8c9c91c775b8980ce9
Christian Lebrio <ChristianLebrio@gmail.com>
2023-02-03 02:09:34 -0500

    solved tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData (nameV, emailV){
+
+    const data = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: nameV,
+          email: emailV,
+        }),
+      };
+      
+      return fetch("http://localhost:3000/users", data)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        document.body.append(object.id);
+      })
+      .catch(function (error) {
+        alert("Fetch Error");
+        document.body.append(error.message);
+      });
+}
